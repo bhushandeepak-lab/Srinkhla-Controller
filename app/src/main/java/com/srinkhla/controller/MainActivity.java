5e6546849ba19ff31afddb5bb66f1419705e2a46
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-16 23:12:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,12 +1,20 @@
 package com.srinkhla.controller;
 import android.app.Activity;
 import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // यह लाइन अब ऐप को चेहरा (Layout) देगी
         setContentView(R.layout.activity_main);
+
+        // ऐप खुलते ही कैमरा और ऑडियो की परमिशन माँगने का कोड
+        String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+        
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(permissions, 100);
+        }
     }
 }
