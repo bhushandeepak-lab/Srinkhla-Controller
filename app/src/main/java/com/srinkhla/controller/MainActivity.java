5cea66a72981a40c1db69a3a120e3550dbd211d0
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-17 00:22:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,31 +1,39 @@
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.srinkhla.controller">
+package com.srinkhla.controller;
 
-    <uses-permission android:name="android.permission.INTERNET" />
-    <uses-permission android:name="android.permission.CAMERA" />
-    <uses-permission android:name="android.permission.RECORD_AUDIO" />
-    
-    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
-    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
-    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />
-    
-    <uses-permission android:name="android.permission.WRITE_SETTINGS" />
-    <uses-permission android:name="android.permission.QUERY_ALL_PACKAGES" />
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
-    <application
-        android:label="Srinkhla Controller"
-        android:theme="@android:style/Theme.DeviceDefault"
-        android:requestLegacyExternalStorage="true">
-        
-        <activity android:name=".MainActivity" android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        <meta-data android:name="moe.shizuku.client.V3_SUPPORT" android:value="true" />
-        
-    </application>
-</manifest>
+        // 1. कैमरा और ऑडियो की परमिशन माँगना
+        String[] permissions = {
+            Manifest.permission.CAMERA, 
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(permissions, 100);
+        }
+
+        // 2. 'All Files Access' (Hard Disk) के लिए सीधा सेटिंग पेज खोलना
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        }
+    }
+}
