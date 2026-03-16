3f9c6b3406e956383d5a43f106d91c6dd75eb84f
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-16 23:04:22 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,30 +1,12 @@
 package com.srinkhla.controller;
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.Manifest;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, 101);
-            if (!Settings.System.canWrite(this)) {
-                Intent i = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                i.setData(Uri.parse("package:" + getPackageName()));
-                startActivity(i);
-            }
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!android.os.Environment.isExternalStorageManager()) {
-                Intent i = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                i.setData(Uri.parse("package:" + getPackageName()));
-                startActivity(i);
-            }
-        }
+        // यह लाइन अब ऐप को चेहरा (Layout) देगी
+        setContentView(R.layout.activity_main);
     }
 }
