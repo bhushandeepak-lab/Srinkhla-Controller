aed19501c5e60ffb9123624a0f72e9081182fcd2
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-16 22:13:04 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,5 +1,4 @@
 package com.srinkhla.controller;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,30 +11,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // 1. फाइल, कैमरा और माइक की परमिशन मांगना
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            }, 101);
-        }
-
-        // 2. सिस्टम सेटिंग बदलने की परमिशन का पेज खोलना
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, 101);
             if (!Settings.System.canWrite(this)) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                startActivity(intent);
+                Intent i = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                i.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(i);
             }
         }
-        
-        // 3. फाइलों का पूरा एक्सेस (Android 11+)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             if (!android.os.Environment.isExternalStorageManager()) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                startActivity(intent);
+                Intent i = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                i.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(i);
             }
         }
     }
