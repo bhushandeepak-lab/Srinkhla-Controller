d3cc0aa74567bfc50c53c09eb3c62ff71b309acf
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-16 13:18:39 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -0,0 +1,12 @@
+package com.srinkhla.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // अभी के लिए खाली छोड़ सकते हैं या कोई बेसिक टेक्स्ट दिखा सकते हैं
+    }
+}
