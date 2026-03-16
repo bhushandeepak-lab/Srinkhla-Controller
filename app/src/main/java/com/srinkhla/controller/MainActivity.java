ff63b56dd2080fc7ff35c3b99bca74c771d93dbe
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-16 13:53:38 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,12 +1,36 @@
 package com.srinkhla.controller;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // अभी के लिए खाली छोड़ सकते हैं या कोई बेसिक टेक्स्ट दिखा सकते हैं
+
+        // 1. कैमरा, ऑडियो और स्टोरेज मांगना
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 101);
+        }
+
+        // 2. मॉडिफाई सिस्टम सेटिंग के लिए पूछना
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                Toast.makeText(this, "Modify Settings परमिशन दें", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
