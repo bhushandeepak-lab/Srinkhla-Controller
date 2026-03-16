3b4c658b7becefdd8024a4dcc21083c0771baac3
bhushandeepak-lab <bhushandeepak@gmail.com>
2026-03-17 00:18:00 +0530

    Update MainActivity.java

--- app/src/main/java/com/srinkhla/controller/MainActivity.java
+++ app/src/main/java/com/srinkhla/controller/MainActivity.java
@@ -1,20 +1,31 @@
-package com.srinkhla.controller;
-import android.app.Activity;
-import android.os.Bundle;
-import android.Manifest;
-import android.content.pm.PackageManager;
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.srinkhla.controller">
 
-public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.CAMERA" />
+    <uses-permission android:name="android.permission.RECORD_AUDIO" />
+    
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />
+    
+    <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+    <uses-permission android:name="android.permission.QUERY_ALL_PACKAGES" />
 
-        // ऐप खुलते ही कैमरा और ऑडियो की परमिशन माँगने का कोड
-        String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+    <application
+        android:label="Srinkhla Controller"
+        android:theme="@android:style/Theme.DeviceDefault"
+        android:requestLegacyExternalStorage="true">
         
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(permissions, 100);
-        }
-    }
-}
+        <activity android:name=".MainActivity" android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+
+        <meta-data android:name="moe.shizuku.client.V3_SUPPORT" android:value="true" />
+        
+    </application>
+</manifest>
